6eb36e77e72e47ad9e3af22c8fb6b1dbd37e33b3
David Binetti <dbinetti@gmail.com>
2021-01-23 06:57:09 -0700

    Update views.py

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,7 @@
 import csv
 
 import requests
+from auth0.v3.authentication.users import Users
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate
@@ -15,6 +16,7 @@ from django.urls import reverse
 from django.utils.crypto import get_random_string
 
 from .forms import DeleteForm
+from .forms import ParentForm
 
 
 # Root
@@ -28,18 +30,15 @@ def index(request):
 
 # Authentication
 def login(request):
-    # Set landing page depending on initial button
-    initial = request.GET.get('initial', 'None')
     redirect_uri = request.build_absolute_uri(reverse('callback'))
-    state = f"{initial}|{get_random_string()}"
+    state = f"{get_random_string()}"
     request.session['state'] = state
-
     params = {
-        'response_type': 'code',
         'client_id': settings.AUTH0_CLIENT_ID,
+        'response_type': 'code',
         'scope': 'openid profile email',
-        'redirect_uri': redirect_uri,
         'state': state,
+        'redirect_uri': redirect_uri,
         'screen_hint': 'signup',
     }
     url = requests.Request(
@@ -56,9 +55,6 @@ def callback(request):
     if browser_state != server_state:
         return HttpResponse(status=400)
 
-    # get initial
-    initial = browser_state.partition("|")[0]
-
     # Get Auth0 Code
     code = request.GET.get('code', None)
     if not code:
@@ -68,15 +64,20 @@ def callback(request):
     token_payload = {
         'client_id': settings.AUTH0_CLIENT_ID,
         'client_secret': settings.AUTH0_CLIENT_SECRET,
-        'redirect_uri': redirect_uri,
         'code': code,
-        'grant_type': 'authorization_code'
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
     }
     token = requests.post(
         token_url,
         json=token_payload,
     ).json()
+    print(token)
     access_token = token['access_token']
+
+    # profile = userinfo(access_token)
+
+
     user_url = f'https://{settings.AUTH0_DOMAIN}/userinfo?access_token={access_token}'
     payload = requests.get(user_url).json()
     # format payload key
@@ -84,21 +85,7 @@ def callback(request):
     user = authenticate(request, **payload)
     if user:
         log_in(request, user)
-        if initial == 'recipient':
-            return redirect('recipient-create')
-        if initial == 'volunteer':
-            return redirect('volunteer-create')
-        if user.is_admin:
-            return redirect('admin:index')
-        recipient = getattr(user, 'recipient', None)
-        volunteer = getattr(user, 'volunteer', None)
-        if recipient and volunteer:
-            return redirect('account')
-        if recipient:
-            return redirect('recipient')
-        if volunteer:
-            return redirect('volunteer')
-        return redirect('account')
+        return redirect('parent')
     return HttpResponse(status=403)
 
 def logout(request):
@@ -118,22 +105,33 @@ def logout(request):
     )
     return redirect(logout_url)
 
-#Account
+# Parent
 @login_required
-def account(request):
-    user = request.user
-    parent = getattr(user, 'parent', None)
+def parent(request):
+    parent = request.user.parent
+    if request.POST:
+        form = ParentForm(request.POST, instance=parent)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request,
+                "Updated!",
+            )
+            return redirect('parent')
+    else:
+        form = ParentForm(instance=parent)
     return render(
         request,
-        'app/pages/account.html',
+        'app/pages/parent.html',
         context={
-            'user': user,
-            'parent': parent,
+            'form': form,
         }
     )
 
+
+# Delete
 @login_required
-def account_delete(request):
+def delete(request):
     if request.method == "POST":
         form = DeleteForm(request.POST)
         if form.is_valid():
@@ -148,95 +146,6 @@ def account_delete(request):
         form = DeleteForm()
     return render(
         request,
-        'app/pages/account_delete.html',
+        'app/pages/delete.html',
         {'form': form,},
     )
-
-# # Parent
-# @login_required
-# def parent(request):
-#     try:
-#         recipient = request.user.recipient
-#     except AttributeError:
-#         return redirect('recipient-create')
-#     assignments = getattr(recipient, 'assignments', None)
-#     return render(
-#         request,
-#         'app/pages/recipient.html',
-#         context={
-#             'recipient': recipient,
-#             'assignments': assignments,
-#         }
-#     )
-
-# @login_required
-# def recipient_create(request):
-#     recipient = getattr(request.user, 'recipient', None)
-#     if recipient:
-#         return redirect('recipient')
-
-#     initial = {
-#         'name': request.user.name,
-#         'email': request.user.email,
-#     }
-#     form = RecipientForm(request.POST) if request.POST else RecipientForm(initial=initial)
-
-#     if form.is_valid():
-#         recipient = form.save(commit=False)
-#         recipient.user = request.user
-#         recipient.save()
-#         send_recipient_confirmation.delay(recipient)
-#         messages.success(
-#             request,
-#             "Registration complete!  We will reach out before November 8th with futher details.",
-#         )
-#         return redirect('recipient')
-#     return render(
-#         request,
-#         'app/pages/recipient_create.html',
-#         context={
-#             'form': form,
-#         }
-#     )
-
-# @login_required
-# def recipient_update(request):
-#     recipient = getattr(request.user, 'recipient', None)
-#     if not recipient:
-#         return redirect('recipient-create')
-#     form = RecipientForm(request.POST, instance=recipient) if request.POST else RecipientForm(instance=recipient)
-#     if form.is_valid():
-#         form.save()
-#         messages.success(
-#             request,
-#             "Recipient information updated!",
-#         )
-#         return redirect('recipient')
-#     return render(
-#         request,
-#         'app/pages/recipient_update.html',
-#         context={
-#             'form': form,
-#         }
-#     )
-
-# @login_required
-# def recipient_delete(request):
-#     if request.method == "POST":
-#         form = DeleteForm(request.POST)
-#         if form.is_valid():
-#             recipient = getattr(request.user, 'recipient', None)
-#             if recipient:
-#                 recipient.delete()
-#             messages.error(
-#                 request,
-#                 "Removed!",
-#             )
-#             return redirect('account')
-#     else:
-#         form = DeleteForm()
-#     return render(
-#         request,
-#         'app/pages/recipient_delete.html',
-#         {'form': form,},
-#     )
